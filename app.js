2a09f475992edd265a884d37b9eeadf038fbda87
AbdulRehman <likeam99@gmail.com>
2022-07-03 16:44:01 +0500

    Get and Populate items

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ const ItemCtrl = (function(){
         currentItem: null,
         totalCalories: 0
     }
+
+     //Public Methods
+        return{
+            getItems: function(){
+                return data.items;
+            }
+        }
     return{
         logData: function(){
             return data;
@@ -31,15 +38,60 @@ const ItemCtrl = (function(){
 
 //UI Controller
 const UICtrl = (function(){
+    const UISelectors = {
+        itemList: '#item-list'
+    }
+
+       //Public Methods
+        return{
+            populateItemsList: function(items){
+                let html = '';
+                items.forEach(function(item){
+                    html += `<li class="collection-item" id="item-${item.id}"> <strong>${item.name}:</strong><em>${item.calories} Calories</em>
+                    <a href="#" class="secondary-content"><i class=" edit-item fa fa-pencil"></i></a>
+                </li>`;
+                });
+
+                //Insert list items
+                document.querySelector(UISelectors.itemList).innerHTML = html;
+            }
+        }
 
 })();
 
 // App Controller
 const AppCtrl = (function(ItemCtrl, UICtrl){
 
+    //Load Event Listener
+    const loadEventListeners = function(){
+
+    //Get UI Selectors
+    const UISelectors = UICtrl.getSelectors();    
+    
+    //Add item event
+    document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+
+    }
+
+
+    //Add item submit
+    const itemAddSubmit = function(e){
+        e.preventDefault();
+    }
+
+
+    //Public Methods
     return{
         init: function(){
             console.log('Intializing the App....');
+
+            //Featch Items from Data Structure
+            const items = ItemCtrl.getItems();
+            
+            //Populate List with items
+            UICtrl.populateItemsList(items);
+
+
         }
     }
 })(ItemCtrl, UICtrl);
